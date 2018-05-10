433929060e3d46bdcebba387598a4b4b3577bb5e
Gruel Wolfgang <gruel@hdm-stuttgart.de>
2018-05-10 20:37:29 +0200

    Added admin pageis to delete and edit stuff

--- admin/phrase_delete.php
+++ admin/phrase_delete.php
@@ -1,20 +1,21 @@
 <?php
-  include('config.php');
+  include('../config.php');
+  $link = mysqli_connect("localhost", "root", "", "i218_phrases_live");
 
-  if (isset($_POST['btn-save'])){
-      $text = "I Say Yes to " . $_POST['phrase'] . "\n";
-      // file_put_contents($filename, $text, FILE_APPEND);
-      $link = mysqli_connect("localhost", "root", "", "i218_phrases_live");
-      $db_query = "INSERT INTO `phrases` (`ID`, `text`, `insertdate`) VALUES (NULL, '" . $text . "', NOW());";
+  if (isset($_GET['delete'])){
+      $db_query = "DELETE FROM `phrases` WHERE `ID` = " . $_GET['delete-id'] ;
       // echo $db_query; 
-      $result = $link->query($db_query);
-      
+      $delete_result = $link->query($db_query);     
+      echo $link->affected_rows; 
   }
+  
+  $result = $link->query('SELECT * FROM phrases');
+
 ?>
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Add phrase</title>
+    <title>Delete phrase</title>
     <meta charset="UTF-8"></meta>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <style type="text/css">
@@ -26,21 +27,27 @@
   <body>
           <div class="jumbotron jumbotron-fluid">
           <div class="container">
-            <h1 class="display-3">I say YES! to ...</h1>
-            <form method="post">
-              <select name="phrase">
-                <option value="ChickenNuggets">Chicken Nuggets</option>
-                <option value="Ketchup">Ketchup</option>
-                <option value="HdM">HdM</option>
-                <option value="Schnittfeld">Schnittfeld</option>
-              </select>
-              <button type="submit" name="btn-save" value="1">Los geht's</button>
-            </form>
+            <h1 class="display-3">DELETE</h1>
+            <table>
+           <?php 
+              // process results
+              while ($row = mysqli_fetch_row($result)){
+                echo "<tr>";
+                echo "<td>" . $row[0] . "</td>";
+                echo "<td>" . $row[1] . "</td>";
+                echo "<td>" . $row[2] . "</td>";
+                echo "<td><a href='?delete=1&delete-id=" . $row[0] . "'>delete </a></td>";
+                echo "<td><a href='phrase_edit.php?edit-id=" . $row[0] . "'> edit</td>";
+                echo "</tr>";
+              }
+           ?>
+            </table>
+
 
           </div>
 
         </div>
-        <?php include('footer.php'); ?>
+        <?php include('../footer.php'); ?>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>    </body>
--- admin/phrase_edit.php
+++ admin/phrase_edit.php
@@ -1,20 +1,24 @@
 <?php
-  include('config.php');
+  include('../config.php');
+  $link = mysqli_connect("localhost", "root", "", "i218_phrases_live");
+  $db_query = "SELECT * FROM `phrases` WHERE `ID` = " . $_GET['edit-id'] ;
+  $result = $link->query($db_query); 
+  $row = mysqli_fetch_row($result); 
+  $text = $row[1];
 
-  if (isset($_POST['btn-save'])){
-      $text = "I Say Yes to " . $_POST['phrase'] . "\n";
-      // file_put_contents($filename, $text, FILE_APPEND);
-      $link = mysqli_connect("localhost", "root", "", "i218_phrases_live");
-      $db_query = "INSERT INTO `phrases` (`ID`, `text`, `insertdate`) VALUES (NULL, '" . $text . "', NOW());";
-      // echo $db_query; 
-      $result = $link->query($db_query);
-      
+  if (isset($_GET['btn-save'])){
+      $db_query = "UPDATE `phrases` SET `text` = '" . $_GET['edit-text'] . "' WHERE `ID` = " . $_GET['edit-id'] ;
+       // echo $db_query; 
+       $update_result = $link->query($db_query);     
   }
+  
+  
+
 ?>
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Add phrase</title>
+    <title>Delete phrase</title>
     <meta charset="UTF-8"></meta>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <style type="text/css">
@@ -26,21 +30,17 @@
   <body>
           <div class="jumbotron jumbotron-fluid">
           <div class="container">
-            <h1 class="display-3">I say YES! to ...</h1>
-            <form method="post">
-              <select name="phrase">
-                <option value="ChickenNuggets">Chicken Nuggets</option>
-                <option value="Ketchup">Ketchup</option>
-                <option value="HdM">HdM</option>
-                <option value="Schnittfeld">Schnittfeld</option>
-              </select>
-              <button type="submit" name="btn-save" value="1">Los geht's</button>
+            <h1 class="display-3">EDIT</h1>
+            <form>
+                <input type="hidden" name="edit-id" value="<?php echo $_GET['edit-id']?>" >
+                <input type="text" name="edit-text" value="<?php echo $text ?>"></input>
+                <button type="submit" name="btn-save" value="1">Update</button>                
             </form>
 
           </div>
 
         </div>
-        <?php include('footer.php'); ?>
+        <?php include('../footer.php'); ?>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>    </body>
